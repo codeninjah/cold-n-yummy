61c4cfa972a12a6f13ea615cc9f669876a140793
codeninjah <codeninjahblog@gmail.com>
2022-03-31 10:33:23 +0200

    Working with rendering of database

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const ejs = require('ejs')
 const { Sequelize, Model, DataTypes } = require("sequelize")
+const {Icecream, Users} = require('./models/index')
 
 
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 
 //This also works with testConnection()
@@ -12,6 +14,25 @@ const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: '../db/icecream.sqlite'
 });
+
+app.get('/', (req, res) => {
+    const iceCreamList = []
+    Icecream.sync()
+        .then(() => {
+            return Icecream.findAll()
+        })
+        .then((data) => {
+            data.forEach((element) => {
+                console.log(element.toJSON())
+                iceCreamList.push(element.toJSON())
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    res.render('index')
+})
 
 async function testConnection(){
     try {
