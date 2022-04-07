bf8ef83f1f3eaedd928ad893b1e329bb85663d9f
codeninjah <codeninjahblog@gmail.com>
2022-04-07 15:45:50 +0200

    Issue #5 SOLVED

--- app2.js
+++ app2.js
@@ -17,24 +17,9 @@ const sequelize = new Sequelize({
     storage: './db/icecream.sqlite'
 });
 
-app.get('/', (req, res) => {
-    const iceCreamList = []
-    Icecream.sync()
-        .then(() => {
-            return Icecream.findAll()
-        })
-        .then((data) => {
-            data.forEach((element) => {
-                console.log(element.toJSON())
-                iceCreamList.push(element.toJSON())
-            })
-            console.log("Längd på array: " + iceCreamList.length)
-            res.render('index', {iceCreamList})
-            return iceCreamList
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+app.get('/', async(req, res) => {
+    const icecreams = await Icecream.findAll()
+    res.render('index', {icecreams})
 })
 
 //Need to work with this
@@ -47,72 +32,29 @@ app.post('/vote', async(req, res) => {
     const user = await Users.findOne({where: {email: email}})
     const icecream = await Icecream.findOne({where: {ice_id: ice_id}})
 
-    // Om user inte finns, då skapas det ett
-    if(user === null){
-        Users.sync()
-        .then(() => {
-            return Users.create({
-                name: username,
-                email: email,
-            })
+    if(!user){
+        const user = await Users.create({
+            name: username,
+            email: email
         })
-        .then((data) => {
-            console.log(data.toJSON())
-            return data
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-    
-        Icecream.sync()
-        .then(() => {
-            return Icecream.findOne({
-                where: {
-                    ice_id: ice_id,
-                }
-            })
-        })
-        .then((data) => {
-                data.likes++
-                data.save()
-                //user.ice_id = data.ice_id
-                //user.save()
-                console.log(data.toJSON())
-            }
-            
-        )
-        .catch((err) => {
-            console.log(err)
-        })        
-}
+
+        icecream.likes++
+        icecream.save()
+    }
+
 res.redirect('/')
 
 })
 
 
-app.get('/top10', (req, res) => {
-    const iceCreamList = []
-    Icecream.sync()
-        .then(() => {
-            return Icecream.findAll({
-                limit: 10,
-                order: sequelize.literal('likes DESC')
-            })
-        })
-        .then((data) => {
-            data.forEach((element) => {
-                console.log(element.toJSON())
-                iceCreamList.push(element.toJSON())
-            })
-            console.log("Längd på array: " + iceCreamList.length)
-            res.render('top10', {iceCreamList})
-            return iceCreamList
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+app.get('/top10', async(req, res) => {
+    const iceCreamList = await Icecream.findAll({
+        limit: 10,
+        order: sequelize.literal('likes DESC')
+    })
+    
+    res.render('top10', {iceCreamList})
 })
-
 
 app.listen(8000, () => {
     console.log("App up and running")
